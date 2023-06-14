d87d5f0f156d0971b96d53c25a5e7a160d02ab8c
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-06-14 13:09:47 -0300

    tentando fazer chamada da API

--- src/components/pokemons/index.js
+++ src/components/pokemons/index.js
@@ -1,60 +1,83 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-async function gerandoPokemons(limite) {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limite}`
-  );
-  return await response.json();
+async function gerandoPokemons() {
+  const limite = 10;
+  const api = "https://pokeapi.co/api/v2/";
+  const response = await fetch(`${api}pokemon?limit=${limite}`);
+  const pokemons = await response.json();
+  const nomesPokemons = pokemons.results.map((pokemon) => {
+    return {
+      nomePokemons: pokemon.name,
+    };
+  });
+  return nomesPokemons;
+}
+
+async function detalhesPokemons(context) {
+  const nomePokemon = context;
+  const api = "https://pokeapi.co/api/v2/";
+  const response = await fetch(`${api}pokemon/${nomePokemon}/`);
+  console.log(nomePokemon);
+  // return await response.json()
 }
 
 export const PokemonsList = () => {
   const [pokemon, setPokemon] = useState({
     testes: [],
+    teste2: [],
   });
 
   useEffect(() => {
     const fetchData = async () => {
-      const limite = 10;
-      const data = await gerandoPokemons(limite);
+      const data = await gerandoPokemons();
+      const pokemons = await detalhesPokemons(data);
 
-      console.log(data.results);
+      // data.map(nomePokemon => {
+      //   console.log(nomePokemon.name);
+      // })
+
+      // console.log('data, ', data);
+      // console.log('pokemon, ', pokemons);
 
       setPokemon({
-        testes: data.results,
+        testes: data,
+        teste2: pokemons,
       });
     };
 
     fetchData();
   }, []);
 
-  const somar = () => {
-    const limite += 10
-    return limite
-  };
+  // const somar = () => {
+  //   const limite += 10
+  //   return limite
+  // };
 
-  const maisPokemons = async () => {
-    const limite = 0
-    const data = await gerandoPokemons(limite)
+  // const maisPokemons = async () => {
+  //   const limite = 0;
+  //   const data = await gerandoPokemons(limite);
 
-    setPokemon({
-        testes: data.results
-    })
-  };
+  //   setPokemon({
+  //     testes: data.results,
+  //   });
+  // };
 
   return (
     <section>
       <h1>Lista de pokemons</h1>
-      <ul>
+      {/* <ul>
         {pokemon.testes.map((teste, index) => {
           return (
             <li key={index}>
-              <p>{teste.name}</p>
-              <p>{teste.url}</p>
+              <Link to={teste.url}>
+                <p>{teste.name}</p>
+              </Link>
             </li>
           );
         })}
-      </ul>
-      <button onClick={() => maisPokemons()}>Ver mais pokemons</button>
+      </ul> */}
+      {/* <button onClick={() => maisPokemons()}>Carregar Mais</button> */}
     </section>
   );
 };
